1476830564ebecd74da30826b6bfe221342af4d7
fkwai <geofkwai@gmail.com>
2017-06-23 01:01:10 -0400

    change a variable name (mask)

---
+++ dataset/script_maskSMAP_CONUS.m
@@ -0,0 +1,28 @@
+
+
+global kPath
+tic
+SMAP_L3=load([kPath.SMAP,'SMAP_L3.mat']);
+toc
+
+%% CONUS Bounding Box
+boundingbox=[-125,-66;25,50];
+indY=find(SMAP_L3.lat>boundingbox(2,1)&SMAP_L3.lat<boundingbox(2,2));
+indX=find(SMAP_L3.lon>boundingbox(1,1)&SMAP_L3.lon<boundingbox(1,2));
+data=SMAP_L3.data(indY,indX,:);
+tnum=SMAP_L3.tnum;
+lat=SMAP_L3.lat(indY);
+lon=SMAP_L3.lon(indX);
+[lonMesh,latMesh]=meshgrid(lon,lat);
+
+%% find valid cells and assign index
+mask=double(~isnan(nanmean(data,3)));
+gridInd=zeros(size(mask));
+gridInd(mask==1)=1:sum(mask(:));
+
+save([kPath.SMAP,'gridSMAP_CONUS.mat'],'mask','gridInd','lat','lon','latMesh','lonMesh')
+%save([kPath.SMAP,'SMAP_L3_CONUS.mat'],'data','gridInd','lat','lon','latMesh','lonMesh','tnum')
+
+
+
+
