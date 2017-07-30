ab81d6294b6b3b60232dc843838b2c239bfca982
Kuai Fang <geofkwai@gmail.com>
2017-07-30 18:15:54 -0400

    add code to read SCAN NLDAS

--- dataset/readNLDAS_Hourly.m
+++ dataset/readNLDAS_Hourly.m
@@ -1,12 +1,12 @@
-function [ data,lat,lon,tnum,field ] = readNLDAS_Hourly(productName,t,fieldind)
+function [ data,lat,lon,tnum,fieldLst ] = readNLDAS_Hourly(productName,t,fieldind)
 %read NLDASH data for given field number, which can be find from
 %wgrib.
 
 % productName - FORA, FORB, NOAH
 % t - time num for a given date
 % fieldInd - the index of field that can be find form wgrib or read_grib(filename,ParamTable,'invent')
-
-% data - grid output 
+%          - if -1 read all field. 
+% data - grid output [ny,nx,nt,nfield]
 % lat,lon,tnum - 1d vector for lat, lon and time (hour, min, second)
 
 global kPath
@@ -14,13 +14,28 @@ global kPath
 switch productName
 	case 'FORA'
 		NLDASdir=[kPath.NLDAS,'NLDAS_FORA0125_H.002',kPath.s];
-		ParamTable=[kPath.NLDAS,'gribtab_NLDAS_FORA_hourly.002.txt'];
-	case 'FORB'
-		NLDASdir=[kPath.NLDAS,'NLDAS_FORB0125_H.002',kPath.s];
-		ParamTable=[kPath.NLDAS,'gribtab_NLDAS_FORB_hourly.002.txt'];
-	case 'NOAH'
-		NLDASdir=[kPath.NLDAS,'NLDAS_NOAH0125_H.002',kPath.s];
-		ParamTable=[kPath.NLDAS,'gribtab_NLDAS_NOAH.002.txt'];
+        ParamTable=[kPath.NLDAS,'gribtab_NLDAS_FORA_hourly.002.txt'];        
+        nField=11;
+    case 'FORB'
+        NLDASdir=[kPath.NLDAS,'NLDAS_FORB0125_H.002',kPath.s];
+        ParamTable=[kPath.NLDAS,'gribtab_NLDAS_FORB_hourly.002.txt'];
+        nField=10;
+    case 'NOAH'
+        NLDASdir=[kPath.NLDAS,'NLDAS_NOAH0125_H.002',kPath.s];
+        ParamTable=[kPath.NLDAS,'gribtab_NLDAS_NOAH.002.txt'];
+        nField=52;
+    case 'VIC'
+        NLDASdir=[kPath.NLDAS,'NLDAS_VIC0125_H.002',kPath.s];
+        ParamTable=[kPath.NLDAS,'gribtab_NLDAS_VIC.002.txt'];
+        nField=43;
+    case 'MOS'
+        NLDASdir=[kPath.NLDAS,'NLDAS_MOS0125_H.002',kPath.s];
+        ParamTable=[kPath.NLDAS,'gribtab_NLDAS_MOS.002.txt'];
+        nField=37;
+end
+
+if fieldind~=-1
+    nField=length(fieldind);
 end
 
 dn=datenumMulti(t,1);
@@ -33,20 +48,32 @@ files = dir([folder,'*.grb']);
 nfiles=length(files);
 tnum=zeros(nfiles,1);
 
-data=zeros(224,464,nfiles)*nan;
+%% read data
+if fieldind~=-1
+    data=zeros(224,464,nfiles)*nan;
+else
+    data=zeros(224,464,nfiles,nField)*nan;
+end
+
 for i=1:nfiles
     filename=[folder,files(i).name];
-%     if ~exist(filename)
-%         error('wrong file name')
-%     end
-%     
+    
     nldas=read_grib(filename,ParamTable,fieldind,'ScreenDiag',0);
-    data1D=nldas.fltarray;
-    datatemp=reshape(data1D,[464,224]);
-    ttemp=datenum(nldas.pds.year,nldas.pds.month,nldas.pds.day,...
-        nldas.pds.hour,nldas.pds.min,0);
-    data(:,:,i)=rot90(datatemp);
+    ttemp=datenum(nldas(1).pds.year,nldas(1).pds.month,nldas(1).pds.day,...
+        nldas(1).pds.hour,nldas(1).pds.min,0);
     tnum(i)=ttemp;
+
+    if fieldind~=-1
+        data1D=nldas.fltarray;
+        datatemp=reshape(data1D,[464,224]);
+        data(:,:,i)=rot90(datatemp);
+    else
+        for k=1:nField
+            data1D=nldas(k).fltarray;
+            datatemp=reshape(data1D,[464,224]);
+            data(:,:,i,k)=rot90(datatemp);
+        end
+    end
 end
 
 % filename=[folder,files(1).name];
@@ -57,7 +84,21 @@ end
 % hard code lat and lon to improve efficiency
 lat=[52.9375:-0.125:25.0625]';
 lon=[-124.9375:0.125:-67.0625];
-field=nldas.parameter;
+
+%% field Name
+fieldLst={nldas.parameter};
+for k=1:length(nldas)
+    if ~strcmp(nldas(k).level,'surface')
+        ind=find(nldas(k).level==' ');
+        levStr=nldas(k).level(1:ind-1);
+        fieldLst{k}=[fieldLst{k},'_',levStr];
+    end
+end
+
+%% possible error
+if length(nldas)~=nField
+    error('wrong number of fields')
+end
 
 end
 
